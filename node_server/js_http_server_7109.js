dadacb4739f193b575b218474b8a8d2fba02dab0
diye <35769340+diyews@users.noreply.github.com>
2023-06-16 14:03:48 +0800

    feat: mousekey server display status

--- node_server/js_http_server_7109.js
+++ node_server/js_http_server_7109.js
@@ -5,15 +5,15 @@ const records = new Array(10)
 
 const recentSecond = 10
 
-let server = http.createServer(function(req, res) {
+let server = http.createServer(function (req, res) {
   if (req.method === 'GET') {
 
     switch (req.url) {
       case '/xxx':
         res.writeHead(200, {
-          'Content-Type': 'text/plain'
+          'Content-Type': 'text/html; charset=utf-8'
         });
-        res.end(formatRecords());
+        res.end(statisticsHTML());
         break;
       case '/favicon.ico':
         res.writeHead(200, {
@@ -55,9 +55,9 @@ function formatRecords() {
       let dateStr = formatDate(o)
 
       if (Date.now() - o < recentSecond * 1e3) {
-        dateStr += ` - In ${recentSecond }s`
+        dateStr += ` - In ${recentSecond}s`
       } else {
-        dateStr += ` - ${Math.floor((Date.now() - o)/1e3)}s`
+        dateStr += ` - ${Math.floor((Date.now() - o) / 1e3)}s`
       }
       return dateStr
     } else {
@@ -65,6 +65,65 @@ function formatRecords() {
     }
   })
 
-  return arr.join('\n')
+  return arr.join('<br>')
 }
+
+function checkIsRunning() {
+  if (records.length) {
+    return Date.now() - records[records.length - 1] < recentSecond * 1e3
+  } else {
+    return false
+  }
+}
+
+function statisticsHTML() {
+  const isRunning = checkIsRunning()
+
+  return `<!doctype html>
+
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Server Status</title>
+    
+    <style>
+      body {
+        background: #000;
+        color: #c7c7c7;
+      }
+
+      .status {
+        display: flex;
+      }
+      .status span {
+        width: 20px;
+        height: 20px;
+        margin-right: 8px;
+        border-radius: 50%;
+        
+        display: inline-flex;
+      }
+      .status .running {
+        background: #0f0;
+      }
+      .status .stop {
+        background: #f00;
+      }
+    </style>
+  </head>
+  
+  <body>
+    <div class="status">${ isRunning ? '<span class="running"></span> Running' : '<span class="stop"></span> Stop' }</div>
+    <div>${formatRecords()}</div>
+    <script>
+      setTimeout(() => {
+        location.reload()
+      }, 30e3)
+    </script>
+  </body>
+  </html>`
+}
+
 server.listen(7109);
